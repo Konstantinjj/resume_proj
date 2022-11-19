b336d718bcd0481030998f8b068fe79ac94481aa
KonstantinPog <k.rogozyanskij@sigma-it.ru>
2022-11-19 17:04:24 +0300

    Lesson 5

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,6 +5,7 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int check = 1;
         for (int i = 0; i < storage.length; i++) {
             if (storage[i] == null) {
                 return;
