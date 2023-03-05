36ce6f04544d9b052720bb2883f3d3435ff2463e
KonstantinRog <k.rogozyanskij@sigma-it.ru>
2023-03-05 16:15:02 +0300

    Lesson 12 Concurrency

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -2,13 +2,28 @@ package com.urise.webapp;
 
 import com.urise.webapp.util.LazySingleton;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MainConcurrency {
     private static int counter;
-    private static final Object LOCK = new Object();
+    private static final AtomicInteger atomicCounter = new AtomicInteger();
+//    private static final Object LOCK = new Object();
+    private static final Lock lock = new ReentrantLock();
+    private static final int THREADS_NUMBER = 10000;
 
+    private static final ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<SimpleDateFormat>(){
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        }
+    };
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
 
@@ -29,30 +44,50 @@ public class MainConcurrency {
 
         System.out.println(thread0.getState());
 
-        List<Thread> threads = new ArrayList<>(10000);
-        for (int i = 0; i < 10000; i++) {
-            Thread thread = new Thread(() -> {
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+//        CompletionService completionService = new ExecutorCompletionService(executorService);
+
+
+//        List<Thread> threads = new ArrayList<>(10000);
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            Future<Integer> future = executorService.submit(() -> {
+
+//            Thread thread = new Thread(() -> {
                 for (int j = 0; j < 100; j++) {
-                    inc();
+                    mainConcurrency.inc();
                 }
+                latch.countDown();
+                return 5;
             });
-            thread.start();
-            threads.add(thread);
+
+//            thread.start();
+//            threads.add(thread);
         }
-        threads.forEach(t -> {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        System.out.println(counter);
+//        threads.forEach(t -> {
+//            try {
+//                t.join();
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        });
+        latch.await(10, TimeUnit.SECONDS);
+        executorService.shutdown();
+//        System.out.println(counter);
+        System.out.println(mainConcurrency.atomicCounter.get());
 
         LazySingleton.getInstance();
     }
 
-    private static synchronized void inc() {
-        counter++;
+    private static void inc() {
+        atomicCounter.incrementAndGet();
+//        lock.lock();
+//        try {
+//            counter++;
+//        } finally {
+//            lock.unlock();
+//        }
     }
 
 //    либо просто private static synchronized, если нет участков, которые надо обрабатывать в очереди
