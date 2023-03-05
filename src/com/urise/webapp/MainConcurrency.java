518fb726f63c6963579550fcbace0ca027a38153
KonstantinRog <k.rogozyanskij@sigma-it.ru>
2023-03-05 17:23:15 +0300

    Lesson 12 HW

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -4,26 +4,29 @@ import com.urise.webapp.util.LazySingleton;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
 
 public class MainConcurrency {
     private static int counter;
     private static final AtomicInteger atomicCounter = new AtomicInteger();
-//    private static final Object LOCK = new Object();
+    //    private static final Object LOCK = new Object();
     private static final Lock lock = new ReentrantLock();
     private static final int THREADS_NUMBER = 10000;
 
-    private static final ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<SimpleDateFormat>(){
+    private static final ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<SimpleDateFormat>() {
         @Override
         protected SimpleDateFormat initialValue() {
             return new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
         }
     };
+
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
 
@@ -77,7 +80,16 @@ public class MainConcurrency {
 //        System.out.println(counter);
         System.out.println(mainConcurrency.atomicCounter.get());
 
-        LazySingleton.getInstance();
+//        LazySingleton.getInstance();
+
+        int[] arr = {1, 2, 3, 3, 2, 3};
+        System.out.println(minValue(arr));
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        System.out.println(oddOrEven(list));
     }
 
     private static void inc() {
@@ -88,6 +100,23 @@ public class MainConcurrency {
 //        } finally {
 //            lock.unlock();
 //        }
+    }
+
+    private static int minValue(int[] values) {
+        final int[] result = {0};
+        Arrays.stream(values).boxed().distinct().sorted().mapToInt(value -> value).forEach(value -> {
+            result[0] = result[0] * 10 + value;
+        });
+        return result[0];
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().mapToInt(value -> value).sum();
+        if (sum % 2 == 0) {
+            return integers.stream().filter(integer -> integer % 2 != 0).collect(Collectors.toList());
+        } else {
+            return integers.stream().filter(integer -> integer % 2 == 0).collect(Collectors.toList());
+        }
     }
 
 //    либо просто private static synchronized, если нет участков, которые надо обрабатывать в очереди
