a7aaa475ce054c5bd94ef7ee6feefdacffccff54
KonstantinRog <k.rogozyanskij@sigma-it.ru>
2023-03-16 12:19:21 +0300

    Lesson 16 JSP

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -21,43 +21,45 @@ public class ResumeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-//        String name = request.getParameter("name");
-//        response.getWriter().write(name == null ? "Hello Resumes" : "Hello " + name + "!");
-        StringBuilder sb = new StringBuilder();
-        sb.append("<table border=\"1\">");
-
-        sb.append("<td>");
-        sb.append("uuid");
-        sb.append("</font>");
-        sb.append("</td>");
-
-        sb.append("<td>");
-        sb.append("full_name");
-        sb.append("</font>");
-        sb.append("</td>");
-
-        sb.append("<tbody>");
-        for (Resume r : storage.getAllSorted()) {
-            sb.append("<tr>");
-
-            sb.append("<td>");
-            sb.append(r.getUuid());
-            sb.append("</td>");
-
-            sb.append("<td>");
-            sb.append(r.getFullName());
-            sb.append("</td>");
-
-            sb.append("</tr>");
-        }
-        sb.append("/<tbody>");
-        sb.append("</table>");
-
-        OutputStream outStream = response.getOutputStream();
-        outStream.write(sb.toString().getBytes(StandardCharsets.UTF_8));
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
+//        request.setCharacterEncoding("UTF-8");
+//        response.setCharacterEncoding("UTF-8");
+//        response.setContentType("text/html; charset=UTF-8");
+////        String name = request.getParameter("name");
+////        response.getWriter().write(name == null ? "Hello Resumes" : "Hello " + name + "!");
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("<table border=\"1\">");
+//
+//        sb.append("<td>");
+//        sb.append("uuid");
+//        sb.append("</font>");
+//        sb.append("</td>");
+//
+//        sb.append("<td>");
+//        sb.append("full_name");
+//        sb.append("</font>");
+//        sb.append("</td>");
+//
+//        sb.append("<tbody>");
+//        for (Resume r : storage.getAllSorted()) {
+//            sb.append("<tr>");
+//
+//            sb.append("<td>");
+//            sb.append(r.getUuid());
+//            sb.append("</td>");
+//
+//            sb.append("<td>");
+//            sb.append(r.getFullName());
+//            sb.append("</td>");
+//
+//            sb.append("</tr>");
+//        }
+//        sb.append("/<tbody>");
+//        sb.append("</table>");
+//
+//        OutputStream outStream = response.getOutputStream();
+//        outStream.write(sb.toString().getBytes(StandardCharsets.UTF_8));
     }
 
     @Override
