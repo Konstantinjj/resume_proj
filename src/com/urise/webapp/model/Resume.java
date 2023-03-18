a97b318bb4941d588b1e8e00232ee472d9283ea5
KonstantinRog <k.rogozyanskij@sigma-it.ru>
2023-03-18 16:09:37 +0300

    Lesson 16 CRUD

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -100,4 +100,8 @@ public class Resume implements Comparable<Resume>, Serializable {
         }
         return comp;
     }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,6 +1,7 @@
 package com.urise.webapp.web;
 
 import com.urise.webapp.Config;
+import com.urise.webapp.model.ContactType;
 import com.urise.webapp.model.Resume;
 import com.urise.webapp.storage.Storage;
 
@@ -21,49 +22,48 @@ public class ResumeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("resumes", storage.getAllSorted());
-        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
-//        request.setCharacterEncoding("UTF-8");
-//        response.setCharacterEncoding("UTF-8");
-//        response.setContentType("text/html; charset=UTF-8");
-////        String name = request.getParameter("name");
-////        response.getWriter().write(name == null ? "Hello Resumes" : "Hello " + name + "!");
-//        StringBuilder sb = new StringBuilder();
-//        sb.append("<table border=\"1\">");
-//
-//        sb.append("<td>");
-//        sb.append("uuid");
-//        sb.append("</font>");
-//        sb.append("</td>");
-//
-//        sb.append("<td>");
-//        sb.append("full_name");
-//        sb.append("</font>");
-//        sb.append("</td>");
-//
-//        sb.append("<tbody>");
-//        for (Resume r : storage.getAllSorted()) {
-//            sb.append("<tr>");
-//
-//            sb.append("<td>");
-//            sb.append(r.getUuid());
-//            sb.append("</td>");
-//
-//            sb.append("<td>");
-//            sb.append(r.getFullName());
-//            sb.append("</td>");
-//
-//            sb.append("</tr>");
-//        }
-//        sb.append("/<tbody>");
-//        sb.append("</table>");
-//
-//        OutputStream outStream = response.getOutputStream();
-//        outStream.write(sb.toString().getBytes(StandardCharsets.UTF_8));
+        String uuid = request.getParameter("uuid");
+        String action = request.getParameter("action");
+        if (action == null) {
+            request.setAttribute("resumes", storage.getAllSorted());
+            request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
+            return;
+        }
+        Resume r;
+        switch (action) {
+            case "delete":
+                storage.delete(uuid);
+                response.sendRedirect("resume");
+                return;
+            case "view":
+            case "edit":
+                r = storage.get(uuid);
+                break;
+            default:
+                throw new IllegalArgumentException("Action " + action + " is illegal");
+        }
+        request.setAttribute("resume", r);
+        request.getRequestDispatcher(
+                ("view".equals(action) ? "/WEB-INF/jsp/view.jsp" : "/WEB-INF/jsp/edit.jsp")
+        ).forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.setCharacterEncoding("UTF-8");
+        String uuid = request.getParameter("uuid");
+        String fullName = request.getParameter("fullName");
+        Resume r = storage.get(uuid);
+        r.setFullName(fullName);
+        for (ContactType type : ContactType.values()) {
+            String value = request.getParameter(type.name());
+            if (value != null && value.trim().length() != 0) {
+                r.addContact(type, value);
+            } else {
+                r.getContacts().remove(type);
+            }
+        }
+        storage.update(r);
+        response.sendRedirect("resume");
     }
 }
